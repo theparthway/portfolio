dab11173a7908e0f2e121ff1dcbfcc4341090781
theparthway <parthpsh@gmail.com>
2023-03-28 01:20:20 +0530

    time table

--- time-table/script.js
+++ time-table/script.js
@@ -9,7 +9,7 @@ const cols = [];
 
 const timings = ['8:25-9:20', '9:25-10:20', '10:35-11:30', '11:35-12:30', '12:35-1:30', '1:35-2:30', '2:35-3:30', '3:35-4:30', '4:35-5:30', '5:35-6:30'];
 const hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const meals = ["Breakfast", "Lunch", "Dinner", "Snacks"];
 
 for (let i=0;i<10;i++) {
@@ -29,10 +29,12 @@ var ist = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
 // let hour = ist.getHours();
 // let minute = ist.getMinutes();
 
-let color1 = "#0a0708";
-let color2 = "#444444";
-let finishedColor = "#747474";
-let currentColor = "#b1b1b1";
+
+
+let color1 = "#d1c5ad";
+let color2 = "#d1c5ad";
+let finishedColor = "black";
+let currentColor = "#444444";
 
 let hour = ist.getHours();
 let minute = ist.getMinutes();
