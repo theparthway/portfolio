989d81693a03a2d51c2f1d5abffd52b4966807d9
theparthway <parthpsh@gmail.com>
2023-06-10 06:12:54 +0530

    first commit

---
+++ invite/script.js
@@ -0,0 +1,13 @@
+const video = document.getElementById("myVideo");
+const btn = document.getElementById("myBtn");
+const text = document.getElementById("text");
+
+function myFunction() {
+    // if (video.paused) {
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+    text.style.display = "block";
+    // btn.style.display = "none";
+}
