7356b5c763d1a661d23f904039e53b66f18c418f
theparthway <parthpsh@gmail.com>
2023-08-12 04:20:23 +0530

    design changes

--- harvested/app.js
+++ harvested/app.js
@@ -1,3 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
